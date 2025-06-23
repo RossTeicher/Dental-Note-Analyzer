a304d43592c9c141b9deb95099593c066aeb27d7
Ross Teicher <your@email.com>
2025-06-22 21:09:22 -0400

    Fresh upload of Module 1

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Dental Note Analyzer - Smart Note Generator", layout="centered")
+
+st.title("🦷 Smart Note Generator - Module 1")
+st.markdown("This module will use patient data and clinical findings to generate legally sound SOAP notes.")
+st.info("Upload patient data or connect to a database to begin generating notes. (Open Dental integration coming soon!)")
